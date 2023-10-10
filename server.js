f5c4332350639bc1b726b57ae8dca4403f346ecb
Yousaf Nabi <yousaf@pactflow.io>
2023-10-10 20:50:48 +0100

    chore: update ports to avoid clash with pact broker

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const cors = require('cors');
 const bodyParser = require('body-parser');
 const routes = require('./src/product/product.routes');
 
-const port = 8000;
+const port = 8001;
 
 const init = () => {
     app.use(bodyParser.json());
